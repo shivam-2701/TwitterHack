4a4398d376ed92f1425f7a7e5d84ce78f3b711ae
shivam-2701 <shivam.shah2701@gmail.com>
2022-08-13 22:16:31 +0530

    View Pager and RecyclerView added

--- app/src/main/java/com/example/twitterhack/FeedAdapter.java
+++ app/src/main/java/com/example/twitterhack/FeedAdapter.java
@@ -4,15 +4,19 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.twitterhack.TweetAdapters.TweetData;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedAdapterViewHolder>{
 
@@ -37,13 +41,17 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedAdapterVie
     @Override
     public void onBindViewHolder(@NonNull FeedAdapterViewHolder holder, int position) {
         TweetData tweetData=mTweetDataList.get(position);
-
-
+        ImagePagerAdapter imagePagerAdapter= new ImagePagerAdapter(tweetData.getMediaUrls());
+        holder.mImageViewPager.setAdapter(imagePagerAdapter);
+        holder.mDescriptionTv.setText(tweetData.getTweet().getText());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if(mTweetDataList==null){
+            return 0;
+        }
+        return mTweetDataList.size();
     }
 
     public class FeedAdapterViewHolder extends RecyclerView.ViewHolder {
@@ -58,4 +66,51 @@ public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedAdapterVie
 
     }
 
+    /*
+       TODO: Finish the PagerAdapter
+     */
+    private class ImagePagerAdapter extends PagerAdapter{
+
+        List<String> mImages=null;
+        public ImagePagerAdapter( List<String> imageUrls){
+            mImages= imageUrls;
+        }
+
+
+        @Override
+        public int getCount() {
+            if(mImages==null){
+                return 0;
+            }
+            return mImages.size();
+        }
+
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == ((ImageView) object);
+        }
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            ((ViewPager) container).removeView((ImageView) object);
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            Context context = container.getContext();
+            ImageView imageView = new ImageView(context);
+            int padding = context.getResources().getDimensionPixelSize(
+                    R.dimen.padding_medium);
+            imageView.setPadding(padding, padding, padding, padding);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            if(mImages!=null && mImages.size()<=position) {
+                Picasso.with(context)
+                        .load(mImages.get(position))
+                        .into(imageView);
+            }
+            ((ViewPager) container).addView(imageView, 0);
+            return imageView;
+        }
+    }
 }
